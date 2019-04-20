70e8a50e5ecf3d50c611367347b8bbd6c3c4d203
Christopher Schnick <c.schnick@monospark.org>
2019-04-20 17:36:32 +0200

    Fix

--- fullGrid/fillGrid.sh
+++
@@ -1,8 +0,0 @@
-./sv -n 10 -d 1 -b 1 -c 2 -s fullGrid -t 1
-./sv -n 10 -d 2 -b 2 -c 4 -s fullGrid -t 1
-./sv -n 10 -d 3 -b 3 -c 6 -s fullGrid -t 1
-./sv -n 10 -d 4 -b 4 -c 7 -s fullGrid -t 1
-./sv -n 10 -d 5 -b 5 -c 7 -s fullGrid -t 100
-./sv -n 10 -d 6 -b 6 -c 7 -s fullGrid -t 128
-./sv -n 10 -d 7 -b 7 -c 7 -s fullGrid -t 128
-./sv -n 10 -d 8 -b 7 -c 7 -s fullGrid -t 128
