655db56531b8ae69e878ebc5a084529d3ba6ed13
Christopher Schnick <c.schnick@monospark.org>
2019-04-23 09:00:06 +0200

    Changes

---
+++ fullGrid-size/fullGrid.sh
@@ -0,0 +1,7 @@
+./sv -n 10 -d 2 -b 2 -c 7 -s fullGrid -t 48
+./sv -n 100 -d 2 -b 2 -c 7 -s fullGrid -t 48
+./sv -n 1000 -d 2 -b 2 -c 7 -s fullGrid -t 48
+./sv -n 10000 -d 2 -b 1 -c 7 -s fullGrid -t 48
+./sv -n 100000 -d 2 -b 1 -c 6 -s fullGrid -t 48
+./sv -n 1000000 -d 2 -b 1 -c 5 -s fullGrid -t 48
+./sv -n 10000000 -d 2 -b 1 -c 4 -s fullGrid -t 48
