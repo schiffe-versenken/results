8c6e4c780015f41cb26f060574ee9f2b8134312c
Christopher Schnick <c.schnick@monospark.org>
2019-05-02 09:34:30 +0200

    Fix parser

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ import sys
 
 directory = sys.argv[1]
 
-fig, axes = plt.subplots(nrows=2, ncols=1, sharex="none", sharey="none", constrained_layout=True)
+fig, axes = plt.subplots(nrows=2, ncols=1, sharex="none", sharey="none")
 
 shipPlot = axes[0]
 shipPlot.set(xlabel=r'$v \; / \; |C_{all}|$', ylabel=r'$P(\mathbf{T}^{\Omega_L}_{strat} \leq v)$', title="Ship distribution")
@@ -17,7 +17,7 @@ fleetPlot = axes[1]
 fleetPlot.yaxis.label.set_size(14)
 fleetPlot.xaxis.label.set_size(14)
 fleetPlot.set(xlabel=r'$v \; / \; |C_{all}|$', ylabel=r'$P(\mathbf{X}^{F_{all}}_{strat} \leq v)$', title="Fleet distribution")
-fleetPlot.set_yscale("log")
+fleetPlot.set_yscale("log", basey=10)
 fleetPlot.yaxis.set_major_formatter(ticker.FormatStrFormatter(r"$2^{-%d}$"))
 
 
@@ -38,32 +38,34 @@ def load_and_plot(file):
         runs = int(file_strings[4])
 
     sample_size = int(int(strings[0]) / runs)
-    ships = int(strings[1])
+    ships = float(strings[1])
     cells = int(strings[2])
 
     xs = [0]
     ys_1 = [0]
     ys_2 = [ships]
     for i in range(3, len(strings) - 2):
-
+        if d > 6 and i % 3 != 0:
+            continue
         v = strings[i].split(',')
         x = int(v[0]) / float(cells)
         xs.append(x)
         y_1 = float(v[1])
         ys_1.append(y_1)
-        y_2 = -int(v[2])
+        y_2 = -float(v[2])
+        if y_2 == 0.0:
+            y_2 = 0.1
         ys_2.append(y_2)
 
     sample_space = r"$\Omega_L=L_{all}$" if ships == sample_size else r"$|\Omega_L|=$" + str(sample_size)
     runs_str = " @ " + str(runs) + " run(s)" if runs > 1 else ""
-    l = shipPlot.step(xs,ys_1, label=name + " n=" + str(n) + ", d=" + str(d) + ", " + sample_space + runs_str)
-    shipPlot.legend(loc='lower right', borderaxespad=0.)
+    dims_str = "0" + str(d) if d < 10 else str(d)
+    l = shipPlot.step(xs,ys_1, where="post", label=name + " n=" + str(n) + ", d=" + dims_str + ", " + sample_space + runs_str)
 
     expected_ships = float(strings[len(strings) - 2])
     shipPlot.axvline(x=(expected_ships / cells), color=l[0].get_color(), linestyle='--')
 
-    l = fleetPlot.step(xs,ys_2, label=name + " n=" + str(n) + ", d=" + str(d) + ", " + sample_space + runs_str)
-    fleetPlot.legend(loc='lower right', borderaxespad=0.)
+    l = fleetPlot.step(xs,ys_2, where="post", label=name + " n=" + str(n) + ", d=" + dims_str + ", " + sample_space + runs_str)
     fleetPlot.axis((0, 1, ships, 0.1))
 
     expected_fleets = float(strings[len(strings) - 1])
@@ -74,4 +76,8 @@ for filename in os.listdir(directory):
     if filename.endswith(".txt"):
         load_and_plot(filename)
 
+handles, labels = shipPlot.get_legend_handles_labels()
+labels, handles = zip(*sorted(zip(labels, handles), key=lambda t: t[0]))
+shipPlot.legend(handles, labels, bbox_to_anchor=(1.01, 1), loc='upper left', borderaxespad=0.)
+fleetPlot.legend(handles, labels, bbox_to_anchor=(1.01, 1), loc='upper left', borderaxespad=0.)
 plt.show()
